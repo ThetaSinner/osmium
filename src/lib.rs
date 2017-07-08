26066b3420d3e742f0abe2cba7b52fa195c01d75
ThetaSinner <greatdjonfire@hotmail.co.uk>
2017-07-08 18:18:38 +0200

    add dependencies

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+// Copyright 2017 ThetaSinner
+//
+// This file is part of Osmium.
+
+// Osmium is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Osmium is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+extern crate bytes;
+extern crate futures;
+extern crate tokio_io;
+extern crate tokio_core;
+extern crate tokio_proto;
+extern crate tokio_service;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
