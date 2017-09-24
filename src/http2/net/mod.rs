36f8513973471b545f222fef99dad3913aa2ff6c
ThetaSinner <greatdjonfire@hotmail.co.uk>
2017-09-25 00:53:31 +0100

    remove test which should be commented
    need to sort that out for real

--- src/http2/net/mod.rs
+++ src/http2/net/mod.rs
@@ -267,7 +267,7 @@ mod tests {
         }
     }
 
-    #[test]
+    // MANUAL TESTING #[test]
     fn test_start_server() {
         println!("start server");
         let handshake = https::HttpsH2Handshake::new();
